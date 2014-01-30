a653331266d5143ae4d7306e69cc0eedae1a5e9c
nerdguy <guynerd56@gmail.com>
2014-01-30 16:24:44 -0600

    Update chaturbateduino_testbed.user.js

--- chaturbateduino_testbed.user.js
+++ chaturbateduino_testbed.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name       Chaturbate
 // @namespace  http://use.i.E.your.homepage/
-// @version    0.1
+// @version    0.2
 // @description  enter something useful
 // @match http://testbed.chaturbate.com/*
 // @copyright  2012+, Nerdguy
 // ==/UserScript==
+
 var main = function () {
     var settings = {
-        API_ROOT: 'http://127.0.0.1:8000/',
+        API_ROOT: 'http://127.0.0.1:8000/v2/',
         PIN: 3,
         CURRENT: 0
     };
@@ -39,7 +40,7 @@ var main = function () {
                     settings.BOARD = data['pk']
                     settings.URL = settings.API_ROOT + 'boards/'+ settings.BOARD + '/' + settings.PIN + '/';
                     $('#firmataPort').removeAttr('disabled');
-                  goal = $('<p><label for="firmataGoal">Goal</label> <input type="integer" id="firmataGoal"></input></p>');
+	                goal = $('<p><label for="firmataGoal">Goal</label> <input type="integer" id="firmataGoal"></input></p>');
     	            $('#firmataGoal', goal).change(function(e) {
                         val = $(this).val();
                         if (val !== undefined && val != '') {
